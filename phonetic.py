717a2ad3bd2b85e2dd560e4ae9a7e351aecedd6a
Eluise0ur <hadaraku416@gmail.com>
2023-06-10 22:00:02 +0900

    Update phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+space = " "
 def read( word ):
     url = f'https://tw.dictionary.search.yahoo.com/search;_ylt=Awrt4EJEZYRkEbkQsgp7rolQ;_ylc=X1MDMTM1MTIwMDM3OQRfcgMyBGZyAwRmcjIDc2ItdG9wLXNlYXJjaARncHJpZANwMXdsM25pWFREeURjZDEuX0pMRFNBBG5fcnNsdAMwBG5fc3VnZwM2BG9yaWdpbgN0dy5kaWN0aW9uYXJ5LnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzMEcXVlcnkDY2F0BHRfc3RtcAMxNjg2Mzk4Mjg0?p={word}&fr=sfp&iscqry=&fr2=sb-top-search'  
     html = requests.get( url )
@@ -9,6 +10,6 @@ def read( word ):
     try:
         english = data.find_all('span')[0].text
         chinese = data.find_all('li')[2].text
-        return( english + chinese )
+        return( english + space + chinese )
     except:
         return( '查無此字' )
